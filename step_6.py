c0e952d2a54d7c8f36521b14b9690a3d6341be24
DongHyun Kim <developerforus@gmail.com>
2022-04-05 21:23:11 +0900

    step_7

--- step_6.py
+++ step_6.py
@@ -92,16 +92,16 @@
 
 # # print(len(word))
 
-case = int(input())
-count = case
+# case = int(input())
+# count = case
 
-for i in range(case) :
-  word = input()
-  for j in range(0, len(word)-1):
-    if word[j] == word[j+1]:
-      pass
-    elif word[j] in word[j+1:]:
-      count -= 1
-      break
+# for i in range(case) :
+#   word = input()
+#   for j in range(0, len(word)-1):
+#     if word[j] == word[j+1]:
+#       pass
+#     elif word[j] in word[j+1:]:
+#       count -= 1
+#       break
 
-print(count)
+# print(count)
---
+++ step_7.py
@@ -0,0 +1,25 @@
+# 노트북 판매 회사
+# 임대료 재산세, 보험료, 급여 A만원 고정비용
+# 재료비와 인건비 B만원 가변 비용
+
+# 노트북 가격 C만원
+
+# A, B, C = map(int, input().split())
+# N = 0
+
+# if (C-B)<= 0:
+#   N = -1
+# else :
+#   N = A/(C-B)+1
+
+# print(int(N))
+
+# N = int(input())
+# distance = 1
+# k = 1
+# while distance < N :
+#   distance = distance + 6 * k
+#   k+=1
+
+# print(k)
+
